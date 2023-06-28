48c6c06e7837eaa96869e83c8618e4411985372b
Arthur <arthur.harkivsky@transportexchangegroup.com>
2023-06-28 23:55:54 +0300

    +

---
+++ detect_and_measure.py
@@ -0,0 +1,76 @@
+import urllib
+
+import cv2
+import matplotlib.pyplot as plt
+import mediapipe as mp
+import numpy as np
+
+
+# noinspection PyUnresolvedReferences
+def url_to_array(url):
+    req = urllib.request.urlopen(url)
+    arr = np.array(bytearray(req.read()), dtype=np.int8)
+    arr = cv2.imdecode(arr, -1)
+    arr = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+    return arr
+
+
+# noinspection PyUnresolvedReferences
+def file_to_array(filepath):
+    arr = cv2.imread(filepath)
+    arr = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+    return arr
+
+
+# noinspection PyUnresolvedReferences,PyUnboundLocalVariable
+def detect_and_measure(filepath):
+    # photo_as_array = url_to_array('https://b2cfurniture.com.au/pub/media/catalog/product/cache/3fb871f48f7af5e44260f2d9fd3932a9/e/l/elm-modern-hardwood-dining-chair-black-hardwood-frame-eco-friendly-beige-fabric_3_.jpg')
+    photo_as_array = file_to_array(filepath)
+    mp_objectron = mp.solutions.objectron
+    mp_drawing = mp.solutions.drawing_utils
+    # Instantiation
+    objectron = mp_objectron.Objectron(
+        static_image_mode=True,
+        max_num_objects=5,
+        min_detection_confidence=0.2,
+        model_name='Chair')
+
+    # Inference
+    results = objectron.process(photo_as_array)
+
+    if not results.detected_objects:
+        print(f'No box landmarks detected.')
+
+    # Copy image so as not to draw on the original one
+    annotated_image = photo_as_array.copy()
+    for detected_object in results.detected_objects:
+        # Draw landmarks
+        mp_drawing.draw_landmarks(annotated_image,
+                                  detected_object.landmarks_2d,
+                                  mp_objectron.BOX_CONNECTIONS)
+
+        # Draw axis based on rotation and translation
+        mp_drawing.draw_axis(annotated_image,
+                             detected_object.rotation,
+                             detected_object.translation)
+    # Plot result
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.imshow(annotated_image)
+    ax.axis('off')
+    print(f'Output photo resolution: {annotated_image.shape[1]} x {annotated_image.shape[0]} pixels')
+    # plt.show()
+
+    return get_sizes(detected_object.landmarks_3d)
+
+
+def get_sizes(landmarks_3d):
+    # Convert landmarks to numpy array
+    landmarks_3d_np = np.array([[lm.x, lm.y, lm.z] for lm in landmarks_3d.landmark])
+
+    # Calculate distances between opposite corners of the box
+    width = np.linalg.norm(landmarks_3d_np[1] - landmarks_3d_np[5]) * 2
+    length = np.linalg.norm(landmarks_3d_np[2] - landmarks_3d_np[6]) * 2
+    height = np.linalg.norm(landmarks_3d_np[4] - landmarks_3d_np[8]) * 2
+    volume = width * length * height
+
+    return width, length, height, volume
--- main.py
+++ main.py
@@ -1,81 +1,26 @@
-import mediapipe as mp
+from flask import *
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-import urllib
+from detect_and_measure import *
 
-
-def url_to_array(url):
-    req = urllib.request.urlopen(url)
-    arr = np.array(bytearray(req.read()), dtype=np.int8)
-    arr = cv2.imdecode(arr, -1)
-    arr = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
-    return arr
+app = Flask(__name__)
 
 
-mug = 'https://goodstock.photos/wp-content/uploads/2018/01/Laptop-Coffee-Mug-on-Table.jpg'
-mug = url_to_array(mug)
-mp_objectron = mp.solutions.objectron
-mp_drawing = mp.solutions.drawing_utils
-# Instantiation
-objectron = mp_objectron.Objectron(
-    static_image_mode=True,
-    max_num_objects=5,
-    min_detection_confidence=0.2,
-    model_name='Cup')
-
-# Inference
-results = objectron.process(mug)
-
-if not results.detected_objects:
-    print(f'No box landmarks detected.')
-
-# Copy image so as not to draw on the original one
-annotated_image = mug.copy()
-for detected_object in results.detected_objects:
-    # Draw landmarks
-    mp_drawing.draw_landmarks(annotated_image,
-                              detected_object.landmarks_2d,
-                              mp_objectron.BOX_CONNECTIONS)
-
-    # Draw axis based on rotation and translation
-    mp_drawing.draw_axis(annotated_image,
-                         detected_object.rotation,
-                         detected_object.translation)
-
-# Plot result
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.imshow(annotated_image)
-ax.axis('off')
-print(f'Output photo resolution: {annotated_image.shape[1]} x {annotated_image.shape[0]} pixels')
+@app.route('/')
+def upload():
+    return render_template("file_upload_form.html")
 
 
-def get_volume(landmarks_3d):
-    # Convert landmarks to numpy array
-    landmarks_3d = np.array([[lm.x, lm.y, lm.z] for lm in landmarks_3d.landmark])
+@app.route('/detect-and-measure', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        width, length, height, volume = detect_and_measure(f.filename)
 
-    # Calculate distances between opposite corners of the box
-    d1 = np.linalg.norm(landmarks_3d[1] - landmarks_3d[5])
-    d2 = np.linalg.norm(landmarks_3d[2] - landmarks_3d[6])
-    d3 = np.linalg.norm(landmarks_3d[4] - landmarks_3d[8])
-    # Calculate volume as product of distances
-    volume = d1 * d2 * d3
-    return volume
-
-
-def get_size(landmarks_3d):
-    # Convert landmarks to numpy array
-    landmarks_3d = np.array([[lm.x, lm.y, lm.z] for lm in landmarks_3d.landmark])
-    # Calculate distances between opposite corners of the box
-    width = np.linalg.norm(landmarks_3d[1] - landmarks_3d[5])
-    length = np.linalg.norm(landmarks_3d[2] - landmarks_3d[6])
-    height = np.linalg.norm(landmarks_3d[4] - landmarks_3d[8])
-    return width, length, height
+        return render_template("success.html", name=f.filename,
+                               width=width, length=length, height=height,
+                               volume=volume)
 
 
-print(
-    f'Volume of detected object: {get_volume(detected_object.landmarks_3d):.4f} m^3 or {get_volume(detected_object.landmarks_3d) * 35.3147:.4f} ft^3')
-print(
-    f'Size of detected object: {get_size(detected_object.landmarks_3d)[0]:.4f} m x {get_size(detected_object.landmarks_3d)[1]:.4f} m x {get_size(detected_object.landmarks_3d)[2]:.4f} m or {get_size(detected_object.landmarks_3d)[0] * 100:.4f} cm x {get_size(detected_object.landmarks_3d)[1] * 100:.4f} cm x {get_size(detected_object.landmarks_3d)[2] * 100:.4f} cm')
-# plt.show()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
